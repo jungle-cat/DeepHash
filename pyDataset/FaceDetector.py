e2fbf3a97a6c704f68f0307af8994944ab50c333
Feng <Feng@Leo>
2014-11-14 22:52:25 +0800

    manuplate youtube faces database

---
+++ pyDataset/FaceDetector.py
@@ -0,0 +1,42 @@
+'''
+Created on Nov 14, 2014
+
+@author: Feng
+'''
+
+import numpy, cv2
+
+class Face(object):
+    def __init__(self, image, rect=None):
+        self.image = image
+        if rect is not None:
+            self.faces = [self.image[r[0]:r[1], r[2]:r[3]] for r in rect]
+        else:
+            self.faces = [self.image]
+        self.faces = [cv2.resize(face, (120,120), interpolation=cv2.INTER_CUBIC) for face in self.faces]
+    
+
+class FaceDetector(object):
+    def __init__(self, face_casfile, eye_casfile=None):
+        self.face_cascade = cv2.CascadeClassifier(face_casfile)
+        if eye_casfile is not None:
+            self.eye_cascade = cv2.CascadeClassifier(eye_casfile)
+        else:
+            self.eye_cascade = None
+    
+    def detect(self, image):
+        if isinstance(image, str):
+            image = cv2.imread(image)
+        
+        if image.ndim == 3:
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
+        else:
+            gray = image
+        
+        faces = self.face_cascade.detectMultiScale(gray, 1.3, 5)
+        sorted_faces = sorted(faces,key=lambda face: face[2]*face[3])
+        
+        return Face(image, sorted_faces)
+        
+            
+            
---
+++ pyDataset/YoutubeFaces.py
@@ -0,0 +1,63 @@
+'''
+Created on Nov 14, 2014
+
+@author: Feng
+'''
+
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+
+import os, random, numpy
+
+from pyDataset.FaceDetector import FaceDetector
+
+def build(base_dir, num_people, num_pfaces, num_max):
+    
+    detector = FaceDetector('haarcascade_frontalface_default.xml')
+    
+    people_names = os.listdir(base_dir)
+    random.shuffle(people_names)
+    
+    # list all posible person names, maximumly n people
+    n = len(people_names)
+    if num_people > 0 and n > num_people:
+        n = num_people
+        people_names = people_names[0:n]
+    
+    face_images = []
+    face_ids = []
+    
+    for i, name in enumerate(people_names):
+        desc_path = os.path.join(base_dir, name)
+        print 'load path %s' % desc_path
+        
+        # list all face image of person name
+        image_names = []
+        for item in os.listdir(desc_path):
+            image_list = [os.path.join(item, im) for im in os.listdir(os.path.join(desc_path, item))]
+            image_names.extend(image_list)
+        random.shuffle(image_names)
+        
+        n_images = len(image_names)
+        if n_images > num_pfaces:
+            n_images = num_pfaces
+            image_names = image_names[0:n_images]
+        
+        
+        # detect faces of image list
+        for image_name in image_names:
+            image_path = os.path.join(desc_path, image_name)
+            face_rets = detector.detect(image_path)
+            if face_rets is not None:
+                face_images.append(face_rets.faces[0])
+                face_ids.append(i+1)
+        
+    # convert list of faces to tensor like numpy.array
+    faces = numpy.asarray(face_images)
+    ids = numpy.asarray(face_ids)
+    
+    return (faces, ids)
+        
+    
