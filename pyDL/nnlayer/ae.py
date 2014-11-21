89353456c1c92dca06456c5af51e1858974e5f92
jungle-cat <ustcrevolutionary@gmail.com>
2014-11-21 23:10:25 +0800

    add autoencoder implementation

---
+++ pyDL/nnlayer/ae.py
@@ -0,0 +1,31 @@
+'''
+Created on Nov 13, 2014
+
+@author: Feng
+'''
+
+
+class AutoEncoder(object):
+    '''
+    '''
+    def __init__(self):
+        pass
+    
+    def encode(self, inputs):
+        '''
+        '''
+        pass
+    
+    def decode(self, hiddens):
+        '''
+        '''
+        pass
+    
+    def reconstruct(self, inputs):
+        '''
+        Reconstuct (decode) the inputs after mapping through encoder
+        
+        :type inputs: 
+        :param inputs: 
+        '''
+        pass
