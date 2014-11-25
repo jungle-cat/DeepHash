a1eb12dfd998f7abeebfd915616513dec97af749
jungle-cat <ustcrevolutionary@gmail.com>
2014-11-25 13:17:02 +0800

    update nnlayer package import

--- pyDL/nnlayer/__init__.py
+++ pyDL/nnlayer/__init__.py
@@ -1,7 +1,10 @@
+from pyDL.nnlayer.nlayer import HiddenLayer
+
 from .ae import AE
 from .dae import DenoisingAE
+
 from .rbm import RBM
 from .grbm import GaussBinaryRBM
+
 from .convlayer import ConvLayer, ConvSampleLayer, MaxSampleLayer
 from .convae import ConvAutoEncoder
-from pyDL.nnlayer.nlayer import HiddenLayer
