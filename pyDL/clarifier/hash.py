395b6c7cc74370f422213753eb10c73154857a20
jungle-cat <ustcrevolutionary@gmail.com>
2014-12-01 12:12:38 +0800

    add hash classifier for Deep Hash and its testcase

---
+++ pyDL/clarifier/hash.py
@@ -0,0 +1,58 @@
+'''
+Created on Dec 1, 2014
+
+@author: Feng
+'''
+
+
+import numpy, theano
+from theano.tensor.shared_randomstreams import RandomStreams
+from theano.tensor.nnet import sigmoid
+from theano import tensor as T
+
+class DeepHashLayer(object):
+    '''
+    '''
+    
+    def __init__(self, inputs, nin, nout, W=None, b=None, numpy_rng=None):
+        '''
+        :type inputs:
+        :param inputs:
+        
+        '''
+        if W is None:
+            initW = numpy.asarray(
+                numpy_rng.uniform(low=-4 * numpy.sqrt(6. / (nin + nout)),
+                                  high=4 * numpy.sqrt(6. / (nin + nout)),
+                                  size=(nin, nout)),
+                dtype=theano.config.floatX
+            )
+            W = theano.shared(value=initW, name='W', borrow=True)
+        if b is None:
+            b = theano.shared(value=numpy.zeros((nout,),
+                                                dtype=theano.config.floatX),
+                              name='b', 
+                              borrow=True
+            )
+        self.inputs = inputs
+        self.W = W
+        self.b = b
+    
+    @property
+    def predict_y(self):
+        return sigmoid(T.dot(self.inputs, self.W) + self.b)
+        
+    def costs(self, y):
+        '''
+        '''
+        # compute similarity matrix S
+        n = y.shape[0]
+        S = y.repeat(T.cast(n,'int16'), axis=0).reshape((n,n)) - y
+        S = T.where(T.eq(S,0), 1., -1.)
+        
+        # compute D-S
+        D = T.diag(T.sum(S,axis=0)) - S
+        cost = T.mean( T.dot( T.dot(self.predict_y, D), self.predict_y.T ) / self.b.shape[0])
+        
+        return cost
+    
---
+++ pyTest/test_hash.py
@@ -0,0 +1,89 @@
+'''
+Created on Dec 1, 2014
+
+@author: Feng
+'''
+
+import unittest, theano, numpy, time, sys, os
+from theano import tensor as T
+from theano.tensor.shared_randomstreams import RandomStreams
+
+from pyDL.nnlayer import AE
+from pyDataset.mnist import MNIST
+from pyDL.clarifier.hash import DeepHashLayer
+
+class AETestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+    
+    def test_AutoEncoder(self):
+        
+        batch_size = 200
+        training_epochs=10
+        
+        # get current file directory and generate
+        curr_dir = os.path.split(__file__)[0]
+        print curr_dir
+        s = os.path.join(curr_dir, '..', '..', 'data','mnist.pkl.gz')
+        print s
+        print os.path.abspath(s)
+        
+        datasets = MNIST(s).load(s)
+        
+        train_set_x, train_set_y = datasets[0]
+        train_set_y = train_set_y + 1
+        
+        # compute number of minibatches for training, validation and testing
+        n_train_batches = train_set_x.get_value(borrow=True).shape[0] / batch_size
+        
+        # allocate symbolic variables for the data
+        index = T.lscalar()    # index to a [mini]batch
+        
+        numpy_rng = numpy.random.RandomState( int(time.time()) )
+        
+        x = T.matrix('x')
+        y = T.ivector('y')
+        
+        dh = DeepHashLayer(x, 28*28, 48, numpy_rng=numpy_rng)
+        cost = dh.costs(y)
+        gparams = [T.grad(cost, param) for param in dh.params]
+        updates = [(param, param-0.01*gparam) for param, gparam in zip(dh.params, gparams)]
+        
+        train_da = theano.function(
+            [index],
+            cost,
+            updates=updates,
+            givens={
+                x: train_set_x[index * batch_size: (index + 1) * batch_size],
+                y: train_set_y[index * batch_size: (index + 1) * batch_size]
+            }
+        )
+        
+        start_time = time.clock()
+    
+        ############
+        # TRAINING #
+        ############
+        
+        # go through training epochs
+        for epoch in xrange(training_epochs):
+            # go through trainng set
+            c = []
+            for batch_index in xrange(n_train_batches):
+                c.append(train_da(batch_index))
+        
+            print 'Training epoch %d, cost %f' % (epoch, numpy.mean(c))
+        
+        end_time = time.clock()
+        
+        training_time = (end_time - start_time)
+        
+        print >> sys.stderr, ('The code for file ' +
+                              os.path.split(__file__)[1] +
+                              ' ran for %.2fm' % ((training_time) / 60.))
+
+if __name__ == '__main__':
+    unittest.main()
